2bba975bf9dd5cefc6360fc51244eb13e532e147
Christopher Mead <chris@mead.one>
2026-02-19 21:50:28 +0000

    Use snapshot of existing keymaps for disabling, rather than preset qwerty ones

--- lua/dvorak-keybinds.lua
+++ lua/dvorak-keybinds.lua
@@ -3,116 +3,190 @@
 
 local M = {}
 
+-- Snapshot mappings before setting them
+M._saved_maps = {}
+
+local function normalise_modes(modes)
+	if type(modes) == "string" then
+		return { modes }
+	end
+	return modes
+end
+
+local function save_map(modes, lhs)
+	modes = normalise_modes(modes)
+
+	M._saved_maps = M._saved_maps or {}
+
+	for _, mode in ipairs(modes) do
+		M._saved_maps[mode] = M._saved_maps[mode] or {}
+
+		if M._saved_maps[mode][lhs] == nil then
+			local found = false
+			for _, map in ipairs(vim.api.nvim_get_keymap(mode)) do
+				if map.lhs == lhs then
+					M._saved_maps[mode][lhs] = map
+					found = true
+					break
+				end
+			end
+
+			if not found then
+				-- Explicitly store false if unmapped
+				M._saved_maps[mode][lhs] = false
+			end
+		end
+	end
+end
+
+-- Create a keymap, but create a snapshot first
+M.map = function(mode, lhs, rhs, opts)
+	save_map(mode, lhs)
+
+	-- if should_apply(lhs, mode) then
+	vim.keymap.set(mode, lhs, rhs, opts)
+	-- end
+end
+
+-- Restore keymaps from snapshot
+M.restore_maps = function()
+	if not M._saved_maps then
+		return
+	end
+
+	for mode, maps in pairs(M._saved_maps) do
+		for lhs, map in pairs(maps) do
+			pcall(vim.keymap.del, mode, lhs)
+
+			if map then
+				vim.keymap.set(
+					mode,
+					lhs,
+					map.rhs({
+						silent = map.silent == 1,
+						expr = map.expr == 1,
+						noremap = map.noremap == 1,
+						nowait = map.nowait == 1,
+						desc = map.desc,
+					})
+				)
+			end
+		end
+	end
+
+	M._saved_maps = {}
+end
+
 -- Keybinds for normal, visual, select, and operator-pending mode
 local function get_global_keybinds()
-  return {
-    -- (t) Navigate down (display lines - same line if wrapped text)
-    { shortcut = "t", command = vim.g.dvorak_visual_line_navigation and "gj" or "j" },
-    -- (n) Navigate up (display lines - same line if wrapped text)
-    { shortcut = "n", command = vim.g.dvorak_visual_line_navigation and "gk" or "k" },
-    -- (s) Navigate right (h) Navigate left
-    { shortcut = "s", command = "l" },
-    -- (h) remains the same
-  }
+	return {
+		-- (t) Navigate down (display lines - same line if wrapped text)
+		{ shortcut = "t", command = M.opts.visual_line_navigation and "gj" or "j" },
+		-- (n) Navigate up (display lines - same line if wrapped text)
+		{ shortcut = "n", command = M.opts.visual_line_navigation and "gk" or "k" },
+		-- (s) Navigate right (h) Navigate left
+		{ shortcut = "s", command = "l" },
+		-- (h) remains the same
+	}
 end
 
 -- Keybinds for normal mode
 local normal_mode_keybinds = {
-  -- (j) Next search result
-  { shortcut = "j", command = "n" },
-  -- (J) Previous search result
-  { shortcut = "J", command = "N" },
-  -- (S) Bottom of screen (H) Top of screen
-  { shortcut = "S", command = "L" },
-  -- (<leader>t{char}) Till character
-  { shortcut = "<leader>t", command = "t" },
-  -- (<leader>T{char}) Till character backwards
-  { shortcut = "<leader>T", command = "T" }
+	-- (j) Next search result
+	{ shortcut = "j", command = "n" },
+	-- (J) Previous search result
+	{ shortcut = "J", command = "N" },
+	-- (S) Bottom of screen (H) Top of screen
+	{ shortcut = "S", command = "L" },
+	-- (<leader>t{char}) Till character
+	{ shortcut = "<leader>t", command = "t" },
+	-- (<leader>T{char}) Till character backwards
+	{ shortcut = "<leader>T", command = "T" },
 }
 
 -- Keybinds for insert mode
 local insert_mode_keybinds = {
-  -- (Ctrl+d) Move cursor left
-  { shortcut = "<C-h>", command = "<Left>" },
-  -- (Ctrl+h) Move cursor down
-  { shortcut = "<C-t>", command = "<Down>" },
-  -- (Ctrl+t) Move cursor up
-  { shortcut = "<C-n>", command = "<Up>" },
-  -- (Ctrl+n) Move cursor right
-  { shortcut = "<C-s>", command = "<Right>" }
+	-- (Ctrl+d) Move cursor left
+	{ shortcut = "<C-h>", command = "<Left>" },
+	-- (Ctrl+h) Move cursor down
+	{ shortcut = "<C-t>", command = "<Down>" },
+	-- (Ctrl+t) Move cursor up
+	{ shortcut = "<C-n>", command = "<Up>" },
+	-- (Ctrl+n) Move cursor right
+	{ shortcut = "<C-s>", command = "<Right>" },
 }
 
 -- Optional keybinds for punctuation line navigation
 local punctuation_line_navigation_keybinds = {
-  -- (-) End of line
-  { shortcut = "-", command = "$" },
-  -- (_) Start of line
-  { shortcut = "_", command = "^" },
+	-- (-) End of line
+	{ shortcut = "-", command = "$" },
+	-- (_) Start of line
+	{ shortcut = "_", command = "^" },
 }
 
 -- Optional keybinds for remapping <C-w> window management commands
 local window_management_keybinds = {
-  -- (Ctrl+w d) Navigate to window left
-  { shortcut = "<C-w>h", command = "<cmd>wincmd h<CR>" },
-  -- (Ctrl+w h) Navigate to window down
-  { shortcut = "<C-w>t", command = "<cmd>wincmd j<CR>" },
-  -- (Ctrl+w t) Navigate to window up
-  { shortcut = "<C-w>n", command = "<cmd>wincmd k<CR>" },
-  -- (Ctrl+w n) Navigate to window right
-  { shortcut = "<C-w>s", command = "<cmd>wincmd l<CR>" },
-  -- (Ctrl+w z) Split window horizontally (replace <C-w>s)
-  { shortcut = "<C-w>z", command = "<cmd>split<CR>" },
+	-- (Ctrl+w d) Navigate to window left
+	{ shortcut = "<C-w>h", command = "<cmd>wincmd h<CR>" },
+	-- (Ctrl+w h) Navigate to window down
+	{ shortcut = "<C-w>t", command = "<cmd>wincmd j<CR>" },
+	-- (Ctrl+w t) Navigate to window up
+	{ shortcut = "<C-w>n", command = "<cmd>wincmd k<CR>" },
+	-- (Ctrl+w n) Navigate to window right
+	{ shortcut = "<C-w>s", command = "<cmd>wincmd l<CR>" },
+	-- (Ctrl+w z) Split window horizontally (replace <C-w>s)
+	{ shortcut = "<C-w>z", command = "<cmd>split<CR>" },
 }
 
 -- Optional keybinds for buffer navigation using the leader key
 local leader_buffer_navigation_keybinds = {
-  { dvorak = "<leader>h", qwerty = "<leader>h", command = "<cmd>bp<CR>" },
-  { dvorak = "<leader>s", qwerty = "<leader>l", command = "<cmd>bn<CR>" }
+	{ dvorak = "<leader>h", qwerty = "<leader>h", command = "<cmd>bp<CR>" },
+	{ dvorak = "<leader>s", qwerty = "<leader>l", command = "<cmd>bn<CR>" },
 }
 
 -- Dvorak keybinds for vim-tmux-navigator
 local vim_tmux_keybinds = {
-  -- (Ctrl+h) Focus window left
-  { dvorak = "<C-h>", qwerty = "<C-h>", command = "<cmd>TmuxNavigateLeft<CR>" },
-  -- (Ctrl+t) Focus window below
-  { dvorak = "<C-t>", qwerty = "<C-j>", command = "<cmd>TmuxNavigateDown<CR>" },
-  -- (Ctrl+n) Focus window above
-  { dvorak = "<C-n>", qwerty = "<C-k>", command = "<cmd>TmuxNavigateUp<CR>" },
-  -- (Ctrl+s) Focus window right
-  { dvorak = "<C-s>", qwerty = "<C-l>", command = "<cmd>TmuxNavigateRight<CR>" },
+	-- (Ctrl+h) Focus window left
+	{ dvorak = "<C-h>", qwerty = "<C-h>", command = "<cmd>TmuxNavigateLeft<CR>" },
+	-- (Ctrl+t) Focus window below
+	{ dvorak = "<C-t>", qwerty = "<C-j>", command = "<cmd>TmuxNavigateDown<CR>" },
+	-- (Ctrl+n) Focus window above
+	{ dvorak = "<C-n>", qwerty = "<C-k>", command = "<cmd>TmuxNavigateUp<CR>" },
+	-- (Ctrl+s) Focus window right
+	{ dvorak = "<C-s>", qwerty = "<C-l>", command = "<cmd>TmuxNavigateRight<CR>" },
 }
 
 -- Keybinds for nvim-tmux-navigation
 local nvim_tmux_keybinds = {
-  -- (Ctrl+h) Focus window left
-  { dvorak = "<C-h>", qwerty = "<C-h>", command = "<cmd>NvimTmuxNavigateLeft<CR>" },
-  -- (Ctrl+t) Focus window below
-  { dvorak = "<C-t>", qwerty = "C-j>", command = "<cmd>NvimTmuxNavigateDown<CR>" },
-  -- (Ctrl+n) Focus window above
-  { dvorak = "<C-n>", qwerty = "<C-k>", command = "<cmd>NvimTmuxNavigateUp<CR>" },
-  -- (Ctrl+s) Focus window right
-  { dvorak = "<C-s>", qwerty = "<C-l>", command = "<cmd>NvimTmuxNavigateRight<CR>" },
+	-- (Ctrl+h) Focus window left
+	{ dvorak = "<C-h>", qwerty = "<C-h>", command = "<cmd>NvimTmuxNavigateLeft<CR>" },
+	-- (Ctrl+t) Focus window below
+	{ dvorak = "<C-t>", qwerty = "C-j>", command = "<cmd>NvimTmuxNavigateDown<CR>" },
+	-- (Ctrl+n) Focus window above
+	{ dvorak = "<C-n>", qwerty = "<C-k>", command = "<cmd>NvimTmuxNavigateUp<CR>" },
+	-- (Ctrl+s) Focus window right
+	{ dvorak = "<C-s>", qwerty = "<C-l>", command = "<cmd>NvimTmuxNavigateRight<CR>" },
 }
 
 -- Keybinds for smart-splits
 local smart_splits_keybinds = {
-  -- (Ctrl+t) Move cursor down
-  { dvorak = "<C-t>", qwerty = "<C-j>", command = "<cmd>lua require('smart-splits').move_cursor_down()<CR>" },
-  -- (Ctrl+n) Move cursor up
-  { dvorak = "<C-n>", qwerty = "<C-k>", command = "<cmd>lua require('smart-splits').move_cursor_up()<CR>" },
-  -- (Ctrl+s) Move cursor right, (Ctrl+h) Move cursor left
-  { dvorak = "<C-s>", qwerty = "<C-l>", command = "<cmd>lua require('smart-splits').move_cursor_right()<CR>" },
-  -- (Ctrl+t) Resize vertical down
-  { dvorak = "<A-t>", qwerty = "<A-j>", command = "<cmd>lua require('smart-splits').resize_down()<CR>" },
-  -- (Ctrl+n) Resize vertical up
-  { dvorak = "<A-n>", qwerty = "<A-k>", command = "<cmd>lua require('smart-splits').resize_up()<CR>" },
-  -- (Ctrl+l) Resize horizontal right, (Ctrl+h) Resize horizontal left
-  { dvorak = "<A-s>", qwerty = "<A-l>", command = "<cmd>lua require('smart-splits').resize_right()<CR>" }
+	-- (Ctrl+t) Move cursor down
+	{ dvorak = "<C-t>", qwerty = "<C-j>", command = "<cmd>lua require('smart-splits').move_cursor_down()<CR>" },
+	-- (Ctrl+n) Move cursor up
+	{ dvorak = "<C-n>", qwerty = "<C-k>", command = "<cmd>lua require('smart-splits').move_cursor_up()<CR>" },
+	-- (Ctrl+s) Move cursor right, (Ctrl+h) Move cursor left
+	{ dvorak = "<C-s>", qwerty = "<C-l>", command = "<cmd>lua require('smart-splits').move_cursor_right()<CR>" },
+	-- (Ctrl+t) Resize vertical down
+	{ dvorak = "<A-t>", qwerty = "<A-j>", command = "<cmd>lua require('smart-splits').resize_down()<CR>" },
+	-- (Ctrl+n) Resize vertical up
+	{ dvorak = "<A-n>", qwerty = "<A-k>", command = "<cmd>lua require('smart-splits').resize_up()<CR>" },
+	-- (Ctrl+l) Resize horizontal right, (Ctrl+h) Resize horizontal left
+	{ dvorak = "<A-s>", qwerty = "<A-l>", command = "<cmd>lua require('smart-splits').resize_right()<CR>" },
 }
 
 -- Check if christoomey/vim-tmux-navigator is installed
 local function tmux_navigator_available()
-  return vim.g.loaded_tmux_navigator == 1 or vim.fn.exists(":TmuxNavigateLeft") > 0
+	return vim.g.loaded_tmux_navigator == 1 or vim.fn.exists(":TmuxNavigateLeft") > 0
 end
 
 -- Check if alexghergh/nvim-tmux-navigation is installed
@@ -126,177 +200,115 @@ local function smart_splits_available()
 end
 
 function M.enable()
-  if vim.g.dvorak_enabled and vim.g.dvorak_enabled == true then
-    return
-  end
-  -- Set global dvorak keybinds
-  for _, mapping in ipairs(get_global_keybinds()) do
-    vim.keymap.set({ "n", "v", "s", "o" }, mapping.shortcut, mapping.command, {noremap = true, silent = true})
-  end
-  -- Set keybinds for normal mode
-  for _, mapping in ipairs(normal_mode_keybinds) do
-    vim.keymap.set("n", mapping.shortcut, mapping.command, {noremap = true, silent = true})
-  end
-  -- Set keybinds for insert mode
-  for _, mapping in ipairs(insert_mode_keybinds) do
-    vim.keymap.set("i", mapping.shortcut, mapping.command, {noremap = true, silent = true})
-  end
+	if vim.g.dvorak_enabled and vim.g.dvorak_enabled == true then
+		return
+	end
+	-- Set global dvorak keybinds
+	for _, mapping in ipairs(get_global_keybinds()) do
+		M.map({ "n", "v", "s", "o" }, mapping.shortcut, mapping.command, { noremap = true, silent = true })
+	end
+	-- Set keybinds for normal mode
+	for _, mapping in ipairs(normal_mode_keybinds) do
+		M.map("n", mapping.shortcut, mapping.command, { noremap = true, silent = true })
+	end
+	-- Set keybinds for insert mode
+	for _, mapping in ipairs(insert_mode_keybinds) do
+		M.map("i", mapping.shortcut, mapping.command, { noremap = true, silent = true })
+	end
 
-  if tmux_navigator_available() then
-    -- Unmap QWERTY bindings for tmux navigation and set Dvorak bindings
-    for _, mapping in ipairs(vim_tmux_keybinds) do
-      pcall(vim.keymap.del, "n", mapping.qwerty)
-      vim.keymap.set("n", mapping.dvorak, mapping.command, {noremap = true, silent = true})
-    end
-  end
+	if tmux_navigator_available() then
+		-- Unmap QWERTY bindings for tmux navigation and set Dvorak bindings
+		for _, mapping in ipairs(vim_tmux_keybinds) do
+			pcall(vim.keymap.del, "n", mapping.qwerty)
+			M.map("n", mapping.dvorak, mapping.command, { noremap = true, silent = true })
+		end
+	end
 
-  if tmux_navigation_available() then
-    -- Unmap QWERTY bindings for nvim-tmux-navigation and set Dvorak bindings
-    for _, mapping in ipairs(nvim_tmux_keybinds) do
-      pcall(vim.keymap.del, "n", mapping.qwerty)
-      vim.keymap.set("n", mapping.dvorak, mapping.command, {noremap = true, silent = true})
-    end
-  end
+	if tmux_navigation_available() then
+		-- Unmap QWERTY bindings for nvim-tmux-navigation and set Dvorak bindings
+		for _, mapping in ipairs(nvim_tmux_keybinds) do
+			pcall(vim.keymap.del, "n", mapping.qwerty)
+			M.map("n", mapping.dvorak, mapping.command, { noremap = true, silent = true })
+		end
+	end
 
-  if smart_splits_available() then
-    -- Unmap QWERTY bindings for smart-splits and set Dvorak bindings
-    for _, mapping in ipairs(smart_splits_keybinds) do
-      pcall(vim.keymap.del, "n", mapping.qwerty)
-      vim.keymap.set("n", mapping.dvorak, mapping.command, {noremap = true, silent = true})
-    end
-  end
+	if smart_splits_available() then
+		-- Unmap QWERTY bindings for smart-splits and set Dvorak bindings
+		for _, mapping in ipairs(smart_splits_keybinds) do
+			pcall(vim.keymap.del, "n", mapping.qwerty)
+			M.map("n", mapping.dvorak, mapping.command, { noremap = true, silent = true })
+		end
+	end
 
-  if vim.g.dvorak_punctuation_line_navigation then
-    for _, mapping in ipairs(punctuation_line_navigation_keybinds) do
-      vim.keymap.set({ "n", "v", "s", "o" }, mapping.shortcut, mapping.command, {noremap = true, silent = true})
-    end
-  end
+	if M.opts.punctuation_line_navigation then
+		for _, mapping in ipairs(punctuation_line_navigation_keybinds) do
+			M.map({ "n", "v", "s", "o" }, mapping.shortcut, mapping.command, { noremap = true, silent = true })
+		end
+	end
 
-  if vim.g.dvorak_window_management then
-    for _, mapping in ipairs(window_management_keybinds) do
-      vim.keymap.set("n", mapping.shortcut, mapping.command, {noremap = true, silent = true})
-    end
-  end
+	if M.opts.window_management then
+		for _, mapping in ipairs(window_management_keybinds) do
+			M.map("n", mapping.shortcut, mapping.command, { noremap = true, silent = true })
+		end
+	end
 
-  if vim.g.dvorak_leader_buffer_navigation then
-    for _, mapping in ipairs(leader_buffer_navigation_keybinds) do
-      pcall(vim.keymap.del, "n", mapping.qwerty)
-      vim.keymap.set("n", mapping.dvorak, mapping.command, {noremap = true, silent = true})
-    end
-  end
+	if M.opts.leader_buffer_navigation then
+		for _, mapping in ipairs(leader_buffer_navigation_keybinds) do
+			pcall(vim.keymap.del, "n", mapping.qwerty)
+			M.map("n", mapping.dvorak, mapping.command, { noremap = true, silent = true })
+		end
+	end
 
-  vim.g.dvorak_enabled = true
-  print("Dvorak keybinds enabled")
+	vim.g.dvorak_enabled = true
+	print("Dvorak keybinds enabled")
 end
 
 function M.disable()
-  if vim.g.dvorak_enabled and vim.g.dvorak_enabled == false then
-    return
-  end
+	if not vim.g.dvorak_enabled then
+		return
+	end
 
-  -- Unmap global dvorak keybinds
-  for _, mapping in ipairs(get_global_keybinds()) do
-    pcall(vim.keymap.del, { "n", "v", "s", "o" }, mapping.shortcut)
-  end
+	M.restore_maps()
 
-  -- Unmap dvorak bindings for normal mode
-  for _, mapping in ipairs(normal_mode_keybinds) do
-    pcall(vim.keymap.del, "n", mapping.shortcut)
-  end
-
-  -- Unmap dvorak bindings for insert mode
-  for _, mapping in ipairs(insert_mode_keybinds) do
-    pcall(vim.keymap.del, "i", mapping.shortcut)
-  end
-
-  -- Unmap Dvorak keybinds for tmux navigation and set QWERTY bindings
-  if tmux_navigator_available() then
-    for _, mapping in ipairs(vim_tmux_keybinds) do
-      pcall(vim.keymap.del, "n", mapping.dvorak)
-      vim.keymap.set("n", mapping.qwerty, mapping.command, {noremap = true, silent = true})
-    end
-  end
-
-  if tmux_navigation_available() then
-    -- Unmap Dvorak keybinds for nvim-tmux-navigation
-    for _, mapping in ipairs(nvim_tmux_keybinds) do
-      pcall(vim.keymap.del, "n", mapping.dvorak)
-      vim.keymap.set("n", mapping.qwerty, mapping.command, {noremap = true, silent = true})
-    end
-  end
-
-  if smart_splits_available() then
-    -- Unmap Dvorak keybinds for smart-splits
-    for _, mapping in pairs(smart_splits_keybinds) do
-      pcall(vim.keymap.del, "n", mapping.dvorak)
-      vim.keymap.set("n", mapping.qwerty, mapping.command, {noremap = true, silent = true})
-    end
-  end
-
-  if vim.g.dvorak_punctuation_line_navigation then
-    for _, mapping in ipairs(punctuation_line_navigation_keybinds) do
-      pcall(vim.keymap.del, { "n", "v", "s", "o" }, mapping.shortcut)
-    end
-  end
-
-  if vim.g.dvorak_window_management then
-    for _, mapping in ipairs(punctuation_line_navigation_keybinds) do
-      pcall(vim.keymap.del, "n", mapping.shortcut)
-    end
-  end
-
-  if vim.g.dvorak_leader_buffer_navigation then
-    for _, mapping in ipairs(leader_buffer_navigation_keybinds) do
-      pcall(vim.keymap.del, "n", mapping.dvorak)
-      vim.keymap.set("n", mapping.qwerty, mapping.command, {noremap = true, silent = true})
-    end
-  end
-
-  vim.g.dvorak_enabled = false
-  print("Dvorak keybinds disabled")
+	vim.g.dvorak_enabled = false
+	print("Dvorak keybinds disabled")
 end
 
 -- Toggle Dvorak keybinds on and off
 function M.toggle()
-  if vim.g.dvorak_enabled and vim.g.dvorak_enabled == true then
-    M.disable()
-  else
-    M.enable()
-  end
+	if vim.g.dvorak_enabled and vim.g.dvorak_enabled == true then
+		M.disable()
+	else
+		M.enable()
+	end
 end
 
 function M.setup(opts)
-  local defaults = {
-    visual_line_navigation = false,
-    punctuation_line_navigation = false,
-    window_management = false,
-    leader_buffer_navigation = false,
-    auto_enable = true,
-  }
+	local defaults = {
+		visual_line_navigation = false,
+		punctuation_line_navigation = false,
+		window_management = false,
+		leader_buffer_navigation = false,
+		auto_enable = true,
+	}
 
-  opts = vim.tbl_deep_extend("force", defaults, opts or {})
+	M.opts = vim.tbl_deep_extend("force", defaults, opts or {})
 
-  vim.g.dvorak_visual_line_navigation = opts.visual_line_navigation
-  vim.g.dvorak_punctuation_line_navigation = opts.punctuation_line_navigation
-  vim.g.dvorak_window_management = opts.window_management
-  vim.g.dvorak_leader_buffer_navigation = opts.leader_buffer_navigation
+	vim.api.nvim_create_user_command("DvorakToggle", function()
+		M.toggle()
+	end, { desc = "Toggle Dvorak keybinds" })
 
-  vim.api.nvim_create_user_command("DvorakToggle", function()
-    M.toggle()
-  end, { desc = "Toggle Dvorak keybinds" })
+	vim.api.nvim_create_user_command("DvorakEnable", function()
+		M.enable()
+	end, { desc = "Enable Dvorak keybinds" })
 
-  vim.api.nvim_create_user_command("DvorakEnable", function()
-    M.enable()
-  end, { desc = "Enable Dvorak keybinds" })
+	vim.api.nvim_create_user_command("DvorakDisable", function()
+		M.disable()
+	end, { desc = "Disable Dvorak keybinds" })
 
-  vim.api.nvim_create_user_command("DvorakDisable", function()
-    M.disable()
-  end, { desc = "Disable Dvorak keybinds" })
-
-  if opts.auto_enable then
-    M.enable()
-  end
+	if opts.auto_enable then
+		M.enable()
+	end
 end
 
 return M
-
